5bb64cf2d08cbba18813bb85fc425539f84090f1
Максим Загорский <you@example.com>
2021-04-06 20:15:53 +0300

    Rubocop use for correction code

--- HomeWork.rb
+++ HomeWork.rb
@@ -1,24 +1,25 @@
+
 class Station
-  attr_reader :name
-  attr_reader :trains
-  #При инициализации класса определяется его имя
+  attr_reader :name, :trains
+
+  # При инициализации класса определяется его имя
   def initialize(name)
     @name = name
-    #Пустой список колличества поездов на станции
+    # Пустой список колличества поездов на станции
     @trains = []
   end
 
-  #Прием поезда
+  # Прием поезда
   def get_train(train)
     @trains.push(train)
   end
 
-  #Возвращение списка поездов на станции по типу
+  # Возвращение списка поездов на станции по типу
   def get_trains_by_type(type)
-    return @trains.select { |item| item.type == type }
+    @trains.select { |item| item.type == type }
   end
 
-  #Удаление поезда со станции
+  # Удаление поезда со станции
   def send_train
     @trains.pop
   end
@@ -27,29 +28,27 @@ end
 class Route
   attr_reader :stations
 
-  #Инициализация с приемом аргументов начальной и конечной станции
+  # Инициализация с приемом аргументов начальной и конечной станции
   def initialize(start_station, end_station)
     @stations = [start_station, end_station]
   end
 
-  #Добавление промежуточной станции в список
+  # Добавление промежуточной станции в список
   def add_station(station)
     @stations.insert(-2, station)
   end
 
-  #Удаление недавно добавленной станции
+  # Удаление недавно добавленной станции
   def del_station(station)
     @stations.delete(station)
   end
-
 end
 
 class Train
   attr_accessor :speed
-  attr_reader :type
-  attr_reader :number
+  attr_reader :type, :number
 
-  #Начальное создание класса с приемом произвольного номера поезда и приемом типа поезда "грузовой" или "пассажирский"
+  # Начальное создание класса с приемом произвольного номера поезда и приемом типа поезда "грузовой" или "пассажирский"
   def initialize(number, type)
     @type = type
     @number = number
@@ -57,34 +56,32 @@ class Train
     @wagon = 0
   end
 
-  #добавление вагона
+  # добавление вагона
   def add_wagon
-    if @speed == 0
+    if @speed.zero?
       @wagon += 1
     else
       puts 'Сначала нужно остановить поезд'
     end
   end
 
-  #Удаление вагона
+  # Удаление вагона
   def del_wagon
-    if @wagon == 0
+    if @wagon.zero?
       puts 'У поезда нет вагонов'
+    elsif @speed.zero?
+      @wagon -= 1
     else
-      if @speed == 0
-        @wagon -= 1
-      else
-        puts 'Сначала нужно остановить поезд'
-      end
+      puts 'Сначала нужно остановить поезд'
     end
   end
 
-  #остановка поезда
+  # остановка поезда
   def stop
     @speed = 0
   end
 
-  #Принятие маршрута
+  # Принятие маршрута
   def submit_route(route, train)
     @route = route
     @number_station = 0
@@ -92,37 +89,34 @@ class Train
     @current_station.get_train(train)
   end
 
-  #Отправление к следующей станции
+  # Отправление к следующей станции
   def go_to_next_station(train)
-    if (@number_station + 1) == @route.stations.length
-      puts 'Конечная станция, ехать можно только назад'
-    else
-      current_station.send_train
-      @number_station += 1
-      current_station.get_train(train)
-    end
+    return unless next_station
+
+    current_station.send_train
+    @number_station += 1
+    current_station.get_train(train)
   end
 
-  #Возвращение на станцию назад
-  def back_to_last_station(train)
-    if @number_station != 0
-      current_station.send_train
-      @number_station -= 1
-      current_station.get_train(train)
-    else
-      puts 'Поезд находится в начальой точке маршрута'
-    end
+  # Возвращение на станцию назад
+  def back_to_previous_station(train)
+    return unless previous_station
+
+    @number_station != 0
+    current_station.send_train
+    @number_station -= 1
+    current_station.get_train(train)
   end
 
   def current_station
-    return @route.stations[@number_station]
+    @route.stations[@number_station]
   end
 
   def previous_station
-    if @number_station == 0
-      puts "Поезд находится на начальной станции"
+    if @number_station.zero?
+      puts 'Поезд находится на начальной станции'
     else
-      return @route.stations[@number_station - 1]
+      @route.stations[@number_station - 1]
     end
   end
 
@@ -130,7 +124,7 @@ class Train
     if (@number_station + 1) == @route.stations.length
       puts 'Поезд находится на конечной станции'
     else
-      return @route.stations[@number_station + 1]
+      @route.stations[@number_station + 1]
     end
   end
 end
