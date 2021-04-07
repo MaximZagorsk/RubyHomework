52078f917bb2778b927f3405345c73ace03d393d
Максим Загорский <you@example.com>
2021-04-07 20:49:26 +0300

    Passenger train

--- passenger_train.rb
+++ passenger_train.rb
@@ -1,1 +1,12 @@
+class PassengerTrain < Train
+  def initialize(number)
+    super
+    @type = "passenger"
+  end
 
+  def add_wagon(wagon)
+    if wagon.type == self.type
+      super
+    end
+  end
+end
